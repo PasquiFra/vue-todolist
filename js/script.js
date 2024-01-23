067073c72686bde69e6d1672c7f753fae0f846a8
Francesco <francesco.pasquinoni@hotmail.it>
2024-01-23 15:48:45 +0100

    VueJS OK

--- js/script.js
+++ js/script.js
@@ -1,2 +1,17 @@
 console.log("Vue ok", Vue);
 
+const { createApp } = Vue;
+
+const app = createApp ({
+    name: "ToDoList",
+    data () {
+        return {
+            message: "Questo messaggio è stato scritto su Javascript"
+        }
+    },
+    /*methods: {
+    
+    }*/
+})
+
+app.mount("#root");
